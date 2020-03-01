f92c32468b4a892f40c41fc9b29714ba093f115e
Wiktor Mińkowski <s16856@pjwstk.edu.pl>
2020-03-01 19:40:09 +0100

    dodano aplikacje

---
+++ cw1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            var content = await response.Content.ReadAsStringAsync();
+            Regex rx = new Regex(@"[a-zA-Z1-9.]+\@[a-zA-Z]+\.[a-zA-Z]+");
+
+            MatchCollection matches = rx.Matches(content);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
